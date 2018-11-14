8c35fbf39afe160b1d8b631f2aab9943570d6bc8
David Smith <shmish111@gmail.com>
2018-11-14 16:20:56 +0000

    use pr for hydra channel for now

--- plutus-playground/deployment/nixops/default.nix
+++ plutus-playground/deployment/nixops/default.nix
@@ -4,7 +4,7 @@ let
   machines = (hostPkgs.lib.importJSON ./machines.json)
   overlays = import ./overlays.nix;
   stdOverlays = [ overlays.journalbeat ];
-  hydraPlayground = import (fetchTarball https://hydra.iohk.io/jobset/plutus/playground/channel/latest/nixexprs.tar.bz2) {};
+  hydraPlayground = import (fetchTarball https://hydra.iohk.io/jobset/Plutus/plutus-PR-302/channel/latest/nixexprs.tar.bz2) {};
   options = { inherit stdOverlays machines defaultMachine hydraPlayground; };
   defaultMachine = (import ./default-machine.nix) options;
   playgroundA = playground.mkInstance options machines.playgroundA;
