9093e09c73117d3e27bbde761a8c27545ce4586b
David Smith <shmish111@gmail.com>
2018-11-15 10:22:32 +0000

    some nixops fixes

--- plutus-playground/deployment/nixops/default.nix
+++ plutus-playground/deployment/nixops/default.nix
@@ -1,12 +1,12 @@
 let
   hostPkgs = import <nixpkgs> {};
-  playground = import ./playground.nix;
+  playgroundMachine = import ./playground.nix;
   machines = (hostPkgs.lib.importJSON ./machines.json);
   overlays = import ./overlays.nix;
   stdOverlays = [ overlays.journalbeat ];
   playground = import ../../../. {};
   options = { inherit stdOverlays machines defaultMachine hydraPlayground; };
   defaultMachine = (import ./default-machine.nix) options;
-  playgroundA = playground.mkInstance options machines.playgroundA;
+  playgroundA = playgroundMachine.mkInstance options machines.playgroundA;
 in
   { inherit playgroundA; }
